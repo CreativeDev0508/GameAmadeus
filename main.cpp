5537e5fecb190fee294e7b2f1a52c9c433ceacbf
Luckyman909 <mingw2816@gmail.com>
2015-10-15 11:46:14 +0200

    Add enemy move prediction

--- main.cpp
+++ main.cpp
@@ -7,9 +7,14 @@
 
 using namespace std;
 
+const int kEnemyPredictVal = -5;
+const int maxX = 30;
+const int maxY = 15;
 
+//int gMap[maxX][maxY] = {0};
 vector<vector<int> > gMap(30, vector<int> (15, 0));
 pair<int, int> gMyPos;
+map<int, pair<int, int>> lastEnemyPos;
 
 enum DIR : int {
 	UP = 0,
@@ -63,6 +68,36 @@ DIR computeDirection(DIR iDir, const pair<int, int> &iCurrentPos, bool&dead)
     return aDir;
 }
 
+void cleanMapPredictions(){
+	for(int i=0;i<maxX; i++)
+		for(int j=0;j<maxY; j++)
+			if(gMap[i][j] == kEnemyPredictVal)
+				gMap[i][j] = 0;
+}
+
+void initLastEnemyPos(int iPlayerCount){
+	for(int i=0;i< iPlayerCount; i++)
+		lastEnemyPos[i]=make_pair(-1, -1);
+}
+
+bool validEnemy(int iX, int iY){
+	return (iX != -1 && iY != -1);
+}
+
+pair<int, int> predictEnemyMove(int iID, pair<int, int> iCurrentPos){
+	pair<int, int> nextMove{0,0};
+	DIR nextDir;
+
+	if(iCurrentPos.first - lastEnemyPos[iID].first)
+		DIR = (iCurrentPos.first - lastEnemyPos[iID].first >= 1 ) ? RIGHT:LEFT;
+	else
+		DIR = (iCurrentPos.second - lastEnemyPos[iID].second >= 1 ) ? DOWN:UP;
+
+	nextMove = getNextPos(nextDir, iCurrentPos);
+	return nextMove;
+}
+
+
 /**
  * Auto-generated code below aims at helping you parse
  * the standard input according to the problem statement.
@@ -71,6 +106,8 @@ int main()
 {
     int playerCount;
     cin >> playerCount; cin.ignore();
+    initLastEnemyPos(playerCount);
+
     int myId;
     cin >> myId; cin.ignore();
 
@@ -78,20 +115,37 @@ int main()
 
     // game loop
     while (1) {
-        int helperBots;
-        cin >> helperBots; cin.ignore();
+    	int helperBots;
+    	cin >> helperBots; cin.ignore();
+
+        // read player trails
         for (int i = 0; i < playerCount; i++) {
             int x;
             int y;
             cin >> x >> y; cin.ignore();
-            if (x==-1 || y==-1)
+            if (validEnemy(x, y))
             	continue;
+
+            cleanMapPredictions();
             gMap[x][y] = i+1;
+
             if(myId == i)
             {
             	gMyPos = make_pair(x, y);
 			}
+            // try to predict next enemy position
+            else if (validEnemy(lastEnemyPos[i].first, lastEnemyPos[i].second))
+            {
+            	pair<int, int> enemyPos = make_pair(x,y);
+            	if (gMap[predictEnemyMove(i, enemyPos).first][predictEnemyMove(i, enemyPos).second] == 0)
+            		gMap[predictEnemyMove(i, enemyPos).first][predictEnemyMove(i, enemyPos).second]= kEnemyPredictVal;
+            }
+            lastEnemyPos[i]=make_pair(x, y);
         }
+
+        // deploy helper bots
+        int helperBots;
+        cin >> helperBots; cin.ignore();
         int removalCount;
         cin >> removalCount; cin.ignore();
         for (int i = 0; i < removalCount; i++) {
